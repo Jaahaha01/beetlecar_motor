b5d9bcf7e2e888c1590eb1cc1be891a2001fecb2
javanesse <javanesse@users.noreply.github.com>
2021-09-19 23:39:49 +0700

    Replace Dropdown to Math Integer Value

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,80 +1,26 @@
 Blockly.defineBlocksWithJsonArray([
 {
   "type": "ultrasonic_read",
+  "lastDummyAlign0": "RIGHT",
   "message0": "Ultrasonic trig pin: %1 echo pin: %2 read distance (cm)",
   "args0": [
     {
-      "type": "field_dropdown",
+      "type": "input_value",
       "name": "trig",
-      "options": [
-        [
-          "OUT1",
-          "26"
-        ],
-        [
-          "OUT2",
-          "27"
-        ],
-        [
-          "23",
-          "23"
-        ],
-        [
-          "19",
-          "19"
-        ],
-        [
-          "18",
-          "18"
-        ],
-        [
-          "VN",
-          "39"
-        ]
-      ]
+      "check": "Number",
+      "align": "RIGHT"
     },
     {
-      "type": "field_dropdown",
+      "type": "input_value",
       "name": "echo",
-      "options": [
-        [
-          "IN1",
-          "32"
-        ],
-        [
-          "IN2",
-          "33"
-        ],
-        [
-          "IN3",
-          "34"
-        ],
-        [
-          "IN4",
-          "35"
-        ],
-        [
-          "23",
-          "23"
-        ],
-        [
-          "19",
-          "19"
-        ],
-        [
-          "18",
-          "18"
-        ],
-        [
-          "VN",
-          "39"
-        ]
-      ]
+      "check": "Number",
+      "align": "RIGHT"
     }
   ],
+  "inputsInline": true,
   "output": "Number",
   "colour": "#004098",
-  "tooltip": "Get data from Sony Remote",
-  "helpUrl": ""
+  "tooltip": "Attach your HC-SR04 to custom Pin",
+  "helpUrl": "https://javanesse.net"
 }
 ]);
--- blocks/generators.js
+++ blocks/generators.js
@@ -1,9 +1,9 @@
 Blockly.Python['ultrasonic_read'] = function(block) {
   Blockly.Python.definitions_['import_ultrasonic'] = 'import Ultrasonic';
-
-  var dropdown_trig = block.getFieldValue('trig');
-  var dropdown_echo = block.getFieldValue('echo');
-
-  var code = `Ultrasonic.read(${dropdown_trig}, ${dropdown_echo})`;
+  
+  var value_trig = Blockly.Python.valueToCode(block, 'trig', Blockly.Python.ORDER_ATOMIC);
+  var value_echo = Blockly.Python.valueToCode(block, 'echo', Blockly.Python.ORDER_ATOMIC);
+  
+  var code = `Ultrasonic.read(${value_trig}, ${value_echo})`;
   return [code, Blockly.Python.ORDER_NONE];
 };
