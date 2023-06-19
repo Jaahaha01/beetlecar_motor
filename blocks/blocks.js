773f25e217bfbc9690567dcba7d09c9a6615a79b
Jaahaha01 <68720175+Jaahaha01@users.noreply.github.com>
2023-06-19 14:19:40 +0700

    Update blocks.js

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,26 +1,265 @@
 Blockly.defineBlocksWithJsonArray([
 {
-  "type": "ultrasonic_read",
-  "lastDummyAlign0": "RIGHT",
-  "message0": "Ultrasonic trig pin: %1 echo pin: %2 read distance (cm)",
-  "args0": [
-    {
-      "type": "input_value",
-      "name": "trig",
-      "check": "Number",
-      "align": "RIGHT"
-    },
-    {
-      "type": "input_value",
-      "name": "echo",
-      "check": "Number",
-      "align": "RIGHT"
-    }
-  ],
-  "inputsInline": true,
-  "output": "Number",
-  "colour": "#004098",
-  "tooltip": "Attach your HC-SR04 to custom Pin",
-  "helpUrl": "https://javanesse.net"
-}
+    "type": "stopMotor",
+    "message0": "stopMotor",
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 240,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "moveAward",
+    "message0": "moveForward for  %1 sec at Speed  %2",
+    "args0": [
+      {
+        "type": "field_number",
+        "name": "moveAward",
+        "value": 0
+      },
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+
+  {
+    "type": "moveBack",
+    "message0": "moveBack for  %1 sec at Speed  %2",
+    "args0": [
+      {
+        "type": "field_number",
+        "name": "moveBack",
+        "value": 0
+      },
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "moveLeft",
+    "message0": "moveLeft for  %1 sec at Speed  %2",
+    "args0": [
+      {
+        "type": "field_number",
+        "name": "moveLeft",
+        "value": 0
+      },
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "moveRight",
+    "message0": "moveRight for  %1 sec at Speed  %2",
+    "args0": [
+      {
+        "type": "field_number",
+        "name": "moveRight",
+        "value": 0
+      },
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "Forward",
+    "message0": "moveForward at Speed  %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "Backward",
+    "message0": "moveBackward at Speed  %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "Left",
+    "message0": "moveLeft at Speed  %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  {
+    "type": "Right",
+    "message0": "moveRight at Speed  %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "speed",
+        "options": [
+          [
+            "50%",
+            "512"
+          ],
+          [
+            "75%",
+            "767"
+          ],
+          [
+            "100%",
+            "1023"
+          ]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+    "tooltip": "",
+    "helpUrl": ""
+  }
 ]);
